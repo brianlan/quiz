a89a95d8d7115e65f5e6ded7cea1311896b4712b
rlan <brianlanbo@gmail.com>
2021-02-28 22:02:52 +0800

    added some helper functions for LinkedList

--- LeetCode/common.py
+++ LeetCode/common.py
@@ -7,6 +7,12 @@ from loguru import logger
 from math import inf
 
 
+class ListNode:
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+
+
 def measure(func):
     def wrapper(*args, **kwargs):
         t0 = time.time()
@@ -14,3 +20,23 @@ def measure(func):
         logger.info(f"function {func.__name__} spent: {(time.time() - t0) * 1000:.2} ms")
         return res
     return wrapper
+
+
+def create_linked_list(nums):
+    if len(nums) == 0:
+        return None
+    head = ListNode(nums[0])
+    p = head
+    for num in nums[1:]:
+        p.next = ListNode(num)
+        p = p.next
+    return head
+
+
+def linked_list_to_nums(head):
+    nums = []
+    p = head
+    while p is not None:
+        nums.append(p.val)
+        p = p.next
+    return nums
