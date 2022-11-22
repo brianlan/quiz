c7487eb627ea51a384399a50fa62aae5f8088b5c
rlan <brianlanbo@gmail.com>
2022-11-22 13:13:03 +0800

    Added solution for LC 572

--- LeetCode/lc_572_subtree_of_another_tree.py
+++ LeetCode/lc_572_subtree_of_another_tree.py
@@ -16,21 +16,42 @@ class TreeNode:
 
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        root_text = Solution.inorder(root, None)
-        subroot_text = Solution.inorder(subRoot, None)
+        root_text = Solution.preorder(root, None)
+        subroot_text = Solution.preorder(subRoot, None)
+        print(root_text)
+        print(subroot_text)
         if root_text.find(subroot_text) == -1:
             return False
         else:
             return True
 
     @staticmethod
-    def inorder(root, src) -> str:
+    def preorder(root, src) -> str:
         if root is None:
             return src
-        return Solution.inorder(root.left, "L") + str(root.val) + Solution.inorder(root.right, "R")
+        return f'_{root.val}_' + Solution.preorder(root.left, "L") + Solution.preorder(root.right, "R")
+
+    @staticmethod
+    def inorder(root, src) -> str:
+        """
+            Can't used in this solution, because the output order of a node's val is not deterministic in DFS.
+            So, different tree structure may output the same string. 
+            For example [4, 1, None, None, 2] and [1, None, 4, 2]
+        """
+        if root is None:
+            return src
+        return Solution.inorder(root.left, "L") + f'_{root.val}_' + Solution.inorder(root.right, "R")
+
+    @staticmethod
+    def postorder(root, src) -> str:
+        if root is None:
+            return src
+        return Solution.postorder(root.left, "L") + Solution.postorder(root.right, "R") + f'_{root.val}_'
 
 
 if __name__ == '__main__':
     assert Solution().isSubtree(create_binary_tree([3,4,5,1,2]), create_binary_tree([4, 1, 2])) is True
     assert Solution().isSubtree(create_binary_tree([3,4,5,1,2,None,None,None,None,0]), create_binary_tree([4, 1, 2])) is False
     assert Solution().isSubtree(create_binary_tree([4,1,None,None,2]), create_binary_tree([1,None,4,2])) is False
+    assert Solution().isSubtree(create_binary_tree([1,None,4,2]), create_binary_tree([4,1,None,None,2])) is False
+    assert Solution().isSubtree(create_binary_tree([12]), create_binary_tree([1])) is False
