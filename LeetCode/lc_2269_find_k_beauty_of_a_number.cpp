663e6729142c11a82edab5240996d22cba8847f8
rlan <brianlanbo@gmail.com>
2025-03-10 20:33:42 +0800

    updated LeetCode/lc_2269_find_k_beauty_of_a_number.cpp

--- LeetCode/lc_2269_find_k_beauty_of_a_number.cpp
+++ LeetCode/lc_2269_find_k_beauty_of_a_number.cpp
@@ -11,14 +11,18 @@ int divisorSubstrings(int num, int k) {
     }
     int k_beauty_cnt = 0;
     string str = to_string(num);
+    int tmp = num;
     int len = str.size();
     for (int i = len ; i >= k ; --i) {
         int divisor = pow(10, i - k);
-        if (num % (num / divisor) == 0) {
+        int substr = tmp / divisor;
+        cout << substr << " ";
+        if ((substr != 0) && (num % substr == 0)) {
             k_beauty_cnt++;
         }
-        num /= 10;
+        tmp %= int(pow(10, i - 1));
     }
+    cout << endl;
     return k_beauty_cnt;
 }
 
